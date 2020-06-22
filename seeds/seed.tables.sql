1a89d2b30e1de460e84bfe8e3903685aad61f013
Cesar Morales <cesarlenin2@live.com>
2020-06-22 08:14:43 -0700

    changed 10 words to learn

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -21,14 +21,16 @@ VALUES
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'bonjour', 'hello', 2),
+  (2, 1, 'au revoir', 'goodbye', 3),
+  (3, 1, 'Merci', 'thank you', 4),
+  (4, 1, 'bonsoir', 'good evening', 5),
+  (5, 1, 'bonne nuit', 'good night', 6),
+  (6, 1, 'excusez-moi', 'excuse me', 7),
+  (7, 1, 'pardon', 'sorry', 8),
+  (8, 1, 'oui', 'yes', 9),
+  (9, 1, 'non', 'no', 10),
+  (10, 1, 'salle de bains', 'bathroom', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
